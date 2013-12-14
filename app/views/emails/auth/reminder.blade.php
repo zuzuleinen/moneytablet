3c9e718098e00dba4a116ce8b9c175d5d99eac4c
Andrei Boar <andrey.boar@gmail.com>
2013-12-14 10:23:06 +0200

    Implemented remind e-mail text and subject

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <h2>Password Reset</h2>
-        <div>
-            To reset your password, complete this form: <a href="{{ URL::to('password/reset', array($token)) }}">{{ URL::to('password/reset', array($token)) }}</a>.
-        </div>
+        <h3>Hi there,</h3>
+        <p>You recently asked to reset your <a href="http://www.moneytablet.com/">moneytablet.com</a> password. If you don't remind doing that, please ignore this e-mail.</p>
+        <p>To reset your password, complete this form: <a href="{{ URL::to('password/reset', array($token)) }}">{{ URL::to('password/reset', array($token)) }}</a>.</p>
     </body>
 </html>
