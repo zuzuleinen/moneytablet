c06439ed6dbf288cc91b549ec6b86b2f9bb9083d
Andrei Boar <andrey.boar@gmail.com>
2014-08-31 15:31:25 +0200

    add last expense

--- app/models/Expense.php
+++ app/models/Expense.php
@@ -2,5 +2,16 @@
 
 class Expense extends Eloquent
 {
-    
+
+    public function getLastExpenses($tabletId, $limit = 1)
+    {
+        return DB::select('SELECT predictions.id, predictions.name, ex.value, ex.created_at
+FROM predictions
+INNER JOIN expenses as ex
+ON predictions.id = ex.prediction_id
+WHERE predictions.tablet_id = ' . $tabletId . '
+ORDER BY ex.created_at DESC
+LIMIT ' . $limit  .';');
+    }
+
 }
